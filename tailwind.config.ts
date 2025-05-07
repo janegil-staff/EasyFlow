10655eb5a16123f3f529d8cb87531d395f7cffc3
Jan Egil Stafff Øvretveit <janstovr@gmail.com>
2025-05-07 16:28:34 +0200

    Initial commit

---
+++ tailwind.config.ts
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./pages/**/*.{html,js}",
+    "./components/**/*.{html,js}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        background: "var(--background)",
+        foreground: "var(--foreground)",
+      },
+    },
+  },
+  plugins: [],
+};
